9fa312bd67bcd75160c4de9e6c94129eeb629b74
vale <valemil.vs@gmail.com>
2020-07-21 23:48:31 +0200

    ..

--- js/myscript.js
+++ js/myscript.js
@@ -3,5 +3,15 @@ var genera = document.getElementById('genera');
 genera.addEventListener("click",
     function () {
     document.getElementById('biglietto').className = "show";
+    var nome = document.getElementById('nome').value;
+    document.getElementById('nome')
+    }
+);
+
+var annulla = document.getElementById('annulla');
+
+annulla.addEventListener("click",
+    function () {
+    document.getElementById('biglietto').className = "hidden";
     }
 );
