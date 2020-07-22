f9f872e5d6d4304e05066ce3237d1a39c1dcfe3e
vale <valemil.vs@gmail.com>
2020-07-22 20:43:55 +0200

    fine esercizio

--- js/myscript.js
+++ js/myscript.js
@@ -4,7 +4,27 @@ genera.addEventListener("click",
     function () {
     document.getElementById('biglietto').className = "show";
     var nome = document.getElementById('nome').value;
-    document.getElementById('nome')
+    document.getElementById('nome-passeggero').innerHTML = nome;
+    var carrozza = generaRandom(1,10);
+    var codiceCp = generaRandom(90000,100000);
+    document.getElementById('carrozza').innerHTML = carrozza;
+    document.getElementById('codice-cp').innerHTML = codiceCp;
+
+    var km = document.getElementById('km').value;
+    var prezzo = km * 0.21;
+    var messaggio = 'Sconto standard'
+
+    var età = document.getElementById('età').value;
+
+    if (età == 'minorenne') {
+        prezzo = prezzo - prezzo * 0.2;
+        var messaggio = 'Sconto minorenne';
+    } else if (età == 'over65') {
+        prezzo = prezzo - prezzo * 0.4;
+        var messaggio = 'Sconto Over';
+    }
+    document.getElementById('offerta').innerHTML = messaggio;
+    document.getElementById('costoBiglietto').innerHTML = prezzo.toFixed(2) + ' €';
     }
 );
 
@@ -15,3 +35,7 @@ annulla.addEventListener("click",
     document.getElementById('biglietto').className = "hidden";
     }
 );
+
+function generaRandom(min,max) {
+    return Math.floor(Math.random()*(max - min) + min);
+}
